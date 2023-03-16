90e533aff60d684a25c950b558cf928c6e0649c3
Mike Malindzisa <machaweml@gmail.com>
2023-03-16 02:00:37 -0700

    Hello, Puts fix line over 80 chars

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - Printing Programming is like building a multilingual puzzle using put command
+ * main - Printing Programming is like building a
+ * multilingual puzzle using put command
  *
  * Return: 0 on success
  */
-int maint(void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
