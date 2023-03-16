c46dd55732cc7c8abe85ca1f59387f7f60b4ac4f
Mike Malindzisa <machaweml@gmail.com>
2023-03-16 01:49:59 -0700

    Hello, Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Printing Programming is like building a multilingual puzzle using put command
+ *
+ * Return: 0 on success
+ */
+int maint(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
