cc80eca43bfa96c42f5602c73b050d5c40d936c0
Mike Malindzisa <machaweml@gmail.com>
2023-03-16 04:23:41 -0700

    Printing the exact quote fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -10,11 +9,10 @@
  *
  * Return: Always 1 (Success)
  */
+
 int main(void)
 {
-	printf("and that piece
-			of art is
-			useful\" - Dora Korpar,
-			2015-10-19");
+	printf("and that piece of art
+	       is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
