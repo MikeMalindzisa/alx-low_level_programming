b4ad6f7857a2c6377acb7fe58d49330802e2219a
Mike Malindzisa <machaweml@gmail.com>
2023-03-16 04:17:50 -0700

    Printing the exact quote fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 
 /**
  * main - Entry point
@@ -16,6 +15,6 @@ int main(void)
 	printf("and that piece
 			of art is
 			useful\" - Dora Korpar,
-			2015-10-19\n", stdout);
+			2015-10-19");
 	return (1);
 }
