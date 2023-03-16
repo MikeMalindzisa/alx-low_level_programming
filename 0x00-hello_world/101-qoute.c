bbf5361b064ece84ed59d81fe569fd5f90bb8dd5
Mike Malindzisa <machaweml@gmail.com>
2023-03-16 03:27:35 -0700

    Printing the exact qoute

---
+++ 0x00-hello_world/101-qoute.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Write a C program that prints
+ * exactly and that piece of art is useful\" -
+ * Dora Korpar, 2015-10-19, followed by a
+ * new line, to the standard error.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (0);
+}
