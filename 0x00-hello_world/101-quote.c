fe864ab2777288d419b00e114725a5fdc922d1ca
Mike Malindzisa <machaweml@gmail.com>
2023-03-16 03:31:21 -0700

    Printing the exact quote fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,6 +13,6 @@
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (0);
 }
