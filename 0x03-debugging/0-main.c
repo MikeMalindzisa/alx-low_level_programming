3d14fd4e368946c9dbad9c28eb960d2b1b3816ab
Mike Malindzisa <machaweml@gmail.com>
2023-03-23 05:24:59 -0700

    0x03. C - Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* main - Start here
+* Desc - Based on the main.c file above, create a file named 0-main.c.
+* This file must test that the function positive_or_negative() gives the
+* correct output when given a case of 0.
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
