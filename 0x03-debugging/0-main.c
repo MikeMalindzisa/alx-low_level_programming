2c8b9529afe9b933b8ed036fa5f8787d17e4b65c
Mike Malindzisa <machaweml@gmail.com>
2023-03-23 05:43:42 -0700

    0. Multiple mains fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
 /**
-* main - Start here
-* Desc - Based on the main.c file above, create a file named 0-main.c.
-* This file must test that the function positive_or_negative() gives the
-* correct output when given a case of 0.
+* main - tests function that prints if integer is positive or negative
 * Return: 0
 */
 
 int main(void)
 {
-	int i;
+        int i;
 
-	i = 0;
-	positive_or_negative(i);
+        i = 0;
+        positive_or_negative(i);
 
-	return (0);
+        return (0);
 }
