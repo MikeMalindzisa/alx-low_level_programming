4e3459fe9f85c05548b004adeb29e2564ada84ec
Mike Malindzisa <machaweml@gmail.com>
2023-04-03 07:09:51 -0700

    0x07. C - Even more pointers, arrays and strings Advanced

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,17 @@
+/**
+ * set_string - Start here
+ * Desc - Write a function that sets the value of a
+ * ointer to a char.
+ *
+ * @s: pointer
+ *
+ * @to: address
+ *
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
