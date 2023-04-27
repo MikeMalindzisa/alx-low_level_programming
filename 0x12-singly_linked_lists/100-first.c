75966799d69572e9edf5fa58c19b7159c290d9f4
MikeMalindzisa <machaweml@gmail.com>
2023-04-27 14:39:31 -0700

    100-first.c Advanced

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,10 +4,6 @@
  */
 void __attribute__((constructor)) before_main()
 {
-	char *part0 = "You're beat! and yet, you must allow,\n";
-	char *part1 = "I bore my house upon my back!\n";
-	size_t len_0 = 39, len_1 = 31;
-
-	fwrite(part0, sizeof(char), len_0, stdout);
-	fwrite(part1, sizeof(char), len_1, stdout);
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }
