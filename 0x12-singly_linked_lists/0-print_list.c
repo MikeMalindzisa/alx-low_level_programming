9f417394b089b09070daeac2e8e2e520bce99dba
MikeMalindzisa <machaweml@gmail.com>
2023-04-27 12:53:15 -0700

    0-print_list.c betty fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,7 +4,7 @@
 /**
  * print_list - Strat here
  * Desc - Prints all elements of list_t
- * @head - pointer to first node
+ * @head: pointer to first node
  *
  * Return: nodes count
  *
@@ -18,14 +18,14 @@ size_t print_list(const list_t *head)
 
 	while (curr_node != NULL)
 	{
-		if (curr_node -> str == NULL)
+		if (curr_node->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n", curr_node -> len, curr_node -> str);
+			printf("[%u] %s\n", curr_node->len, curr_node->str);
 
 		i++;
-		curr_node = curr_node -> next;
+		curr_node = curr_node->next;
 	}
 
-	return i;
+	return (i);
 }
