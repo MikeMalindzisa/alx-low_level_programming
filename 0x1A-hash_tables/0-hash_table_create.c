0a1a3ee8cb1987f9c805eb6950cf9dc24bc9d3fd
MikeMalindzisa <machaweml@gmail.com>
2023-09-12 20:30:15 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,46 +1,29 @@
 #include "hash_tables.h"
-
 /**
-* hash_table_create - Creates a new hash table with the given size.
-* @size: The size of the hash table array.
+* hash_table_create - function
+* @size: size of the hash table array
 *
 * Description: Creates a hash table.
-* Return: A pointer to the newly created hash table, or NULL on failure.
+* Return: Ptr to new table, or NULL if fail
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
+	unsigned long int index;
 
-	if (size <= 0)
+	if (size <= 0) /* size must be a positive number */
 		return (NULL);
 
+/* allocate memory for table and array and check for fail */
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
-
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
-	{
-		free(new_table);
 		return (NULL);
-	}
-
 	new_table->size = size;
-	initialize_array(new_table->array, size);
-
+/* traverse size and set each array index to null inside table */
+	for (index = 0; index < size; index++)
+		new_table->array[index] = NULL;
 	return (new_table);
 }
-
-/**
-* initialize_array - Initializes each index of a hash table array to NULL.
-* @array: The array to initialize.
-* @size: The size of the array.
-*/
-void initialize_array(hash_node_t **array, unsigned long int size)
-{
-	unsigned long int index;
-
-	for (index = 0; index < size; index++)
-		array[index] = NULL;
-}
-
