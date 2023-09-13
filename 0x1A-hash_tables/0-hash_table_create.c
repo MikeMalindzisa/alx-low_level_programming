2cb9fe2dfe6a6a8fe8936c04d6832f33a071655c
MikeMalindzisa <machaweml@gmail.com>
2023-09-12 19:57:19 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,38 +1,45 @@
 #include "hash_tables.h"
 
 /**
-* print_malloc_error - Prints a malloc error message to stderr.
-*/
-void print_malloc_error(void)
-{
-	fprintf(stderr, "Error: malloc failed\n");
-}
-
-/**
-* hash_table_create - Creates a hash table.
-* @size: the size, in the number of nodes, to make the new hash table.
+* hash_table_create - Creates a new hash table with the given size.
+* @size: The size of the hash table array.
 *
-* Return: a pointer to the newly created hash table, or NULL on failure.
+* Description: Creates a hash table.
+* Return: A pointer to the newly created hash table, or NULL on failure.
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hashTable = malloc(sizeof(hash_table_t));
+	hash_table_t *new_table;
 
-	if (hashTable == NULL)
+	if (size <= 0)
+		return (NULL);
+
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
+
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
 	{
-		print_malloc_error();
+		free(new_table);
 		return (NULL);
 	}
 
-	hashTable->size = size;
-	hashTable->array = calloc(size, sizeof(hash_node_t *));
+	new_table->size = size;
+	initialize_array(new_table->array, size);
 
-	if (hashTable->array == NULL)
-	{
-		print_malloc_error();
-		free(hashTable);
-		return (NULL);
+	return (new_table);
 	}
 
-	return (hashTable);
+/**
+* initialize_array - Initializes each index of a hash table array to NULL.
+* @array: The array to initialize.
+* @size: The size of the array.
+*/
+void initialize_array(hash_node_t **array, unsigned long int size)
+{
+	unsigned long int index;
+
+	for (index = 0; index < size; index++)
+		array[index] = NULL;
 }
